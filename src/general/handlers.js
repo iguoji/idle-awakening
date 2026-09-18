e0e05b96aa7e28ccbe2320afe6ace3b6921f3e17
iGuoji <asgeg@qq.com>
2026-09-18 18:18:38 +0800

    回滚：移除不适合作为源码的 bundle 包装内容

--- src/general/handlers.js
+++
@@ -1,7 +0,0 @@
-__webpack_require__.r(__webpack_exports__);
-// export __webpack_require__.d(__webpack_exports__, {
-// export   globalEventHandlers: () => (/* binding */ globalEventHandlers)
-// export });
-var globalEventHandlers = {};
-
-//# sourceURL=webpack://idlemancery-v2-reworked/./src/general/handlers.js?
